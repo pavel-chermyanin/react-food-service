44cbe9bb7db86ebad0b5619a62bbe0049da4a180
Pavel <pavel_chermyanin@mail.ru>
2022-05-12 22:33:10 +0500

    created Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+
+
+
+const Search = ({ cb = Function.prototype }) => {
+    const [value, setValue] = useState('')
+
+    const handleKey = (e) => {
+        if (e.key === 'Enter') {
+            handleSubmit()
+        }
+    }
+    const handleSubmit = () => {
+        cb(value)
+    }
+
+    return (
+        <div className="row">
+            <div className="input-field col s12">
+                <input
+                    type="search"
+                    id="search-field"
+                    placeholder="search"
+                    onKeyDown={handleKey}
+                    onChange={(e) => setValue(e.target.value)}
+                    value={value}
+                />
+                <button
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        right: 0
+                    }}
+                    onClick={handleSubmit}
+                    className="btn">Search</button>
+            </div>
+        </div>
+    )
+}
+
+export default Search
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,46 @@
 
 
 import { useEffect, useState } from 'react';
+import {  useLocation } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 import { getAllCategories } from '../api';
 import CategoryList from '../components/CategoryList';
 import { Preloader } from '../components/Preloader';
+import Search from '../components/Search';
 
 
 const Home = () => {
   const [catalog, setCatalog] = useState([])
+  const [filteredCatalog, setFilteredCatalog] = useState([])
 
+  const {pathname, search} = useLocation()
+  const {push} = useHistory()
+  console.log(pathname, search);
+
+
+  const handleSearch = (str) => {
+    setFilteredCatalog(
+      catalog.filter(item => item.strCategory.toLowerCase().includes(str.toLowerCase()))
+    )
+    push({
+      pathname,
+      search: `?search=${str}`
+    })
+  }
 
   useEffect(() => {
     getAllCategories()
-      .then(response => setCatalog(response.categories))
+      .then(response => {
+        setCatalog(response.categories)
+        setFilteredCatalog(response.categories)
+      })
   }, [])
 
   return (
     <>
+      <Search cb={handleSearch} />
       {!catalog.length ? <Preloader /> : (
-        <CategoryList catalog={catalog}/>
+        <CategoryList catalog={filteredCatalog} />
       )}
     </>
   )
